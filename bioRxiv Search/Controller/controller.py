c717b9211487dfff63e9c3e88b2b16b3959ae886
Daniel Granados <dandiegogranados@yahoo.com>
2023-08-09 20:56:22 -0600

    Crawler
    Se inició con la implementación de elastic search al controller y al crawler.

--- bioRxiv Search/Controller/controller.py
+++ bioRxiv Search/Controller/controller.py
@@ -3,10 +3,12 @@ import pika
 import os
 import json
 import math
+import time
+from elasticsearch import Elasticsearch
 
 def get_biorxiv_data():
     base_url = 'https://api.biorxiv.org'
-    endpoint = '/covid19/0'
+    endpoint = '/covid19/30'
     headers = {'Content-Type': 'application/json'}
 
     response = requests.get(base_url + endpoint, headers=headers)
@@ -21,9 +23,12 @@ RABBIT_MQ=os.getenv('RABBITMQ')
 RABBIT_MQ_PASSWORD=os.getenv('RABBITPASS')
 CRAWLER_QUEUE=os.getenv('CRAWLER_QUEUE')
 
-# credentials = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
-# parameters = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials)
-parameters = pika.ConnectionParameters(host=RABBIT_MQ)
+ESENDPOINT=os.getenv('ESENDPOINT')
+ESPASSWORD=os.getenv('ESPASSWORD')
+ESINDEX=os.getenv('ESINDEX')
+
+credentials = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
+parameters = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 channel.queue_declare(queue=CRAWLER_QUEUE, durable = True)
@@ -35,13 +40,22 @@ data = {
 }
 jsonexample = json.dumps(data)
 
+client = Elasticsearch("http://"+ESENDPOINT+":9200", basic_auth=("elastic", ESPASSWORD), verify_certs=False)
+
+search_body = {
+    "size": 1
+}
+
 while True:
     # Revisa a que recibe el request del kibana service.
-    jsonread = json.loads(jsonexample)
+    jsonread = client.search(index="jobs", body = search_body)
+
     pageSize = int(jsonread["pageSize"]) # extrae el page size del índice
     apiData = get_biorxiv_data()
     total = int(apiData["messages"][0]["total"])
     print(total)
+    print(jsonread)
+
     splits = math.ceil(total / pageSize)
     for split in range(splits):
         jsonread["splitNumber"] = split
--- bioRxiv Search/crawler/crawler.py
+++ bioRxiv Search/crawler/crawler.py
@@ -2,14 +2,16 @@ import requests
 import pika
 import os
 import json
+from elasticsearch import Elasticsearch
 
-def get_biorxiv_data(query=None):
+def get_biorxiv_data(offset=None):
     base_url = 'https://api.biorxiv.org'
-    endpoint = '/covid19/0'
+    endpoint = '/covid19/'
     headers = {'Content-Type': 'application/json'}
 
-    response = requests.get(base_url + endpoint, headers=headers)
-
+    response = requests.get(base_url + endpoint + str(offset), headers=headers)
+    print(base_url + endpoint + str(offset))
+    #print(response.json())
     if response.status_code == 200:
         return response.json()
     else:
@@ -19,9 +21,24 @@ def get_biorxiv_data(query=None):
 def callback(ch, method, properties, body):
     json_object = json.loads(body)
     print(f"Received {json_object}")
+    jobId = json_object["jobId"]
+    pageSize = int(json_object["pageSize"])
+    splitNumber = int(json_object["splitNumber"])
+    articles = []
+    articleNumber = pageSize * splitNumber
+    count = 0
+    while(count < pageSize):
+        data = get_biorxiv_data(articleNumber + count)
+        articlesDownloaded = min(len(data["collection"]), pageSize - count)
+        articles += data["collection"][:articlesDownloaded]
+        count += articlesDownloaded
+
     json_object["status"] = 'DOWNLOADED'
+    documentId = jobId + str(splitNumber)
+    articlesJson = {documentId: articles}
     channel.basic_publish(exchange='', routing_key=SPACY_QUEUE, body=json.dumps(json_object))
-    print(json_object)
+    print(splitNumber, len(articles))
+    resp = client.index(index=ESINDEX, id=documentId, document=json.dumps(articlesJson))
 
 
 RABBIT_MQ=os.getenv('RABBITMQ')
@@ -29,14 +46,19 @@ RABBIT_MQ_PASSWORD=os.getenv('RABBITPASS')
 CRAWLER_QUEUE=os.getenv('CRAWLER_QUEUE')
 SPACY_QUEUE=os.getenv('SPACY_QUEUE')
 
-# credentials = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
-# parameters = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials)
-parameters = pika.ConnectionParameters(host=RABBIT_MQ)
+ESENDPOINT=os.getenv('ESENDPOINT')
+ESPASSWORD=os.getenv('ESPASSWORD')
+ESINDEX=os.getenv('ESINDEX')
+
+credentials = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
+parameters = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 channel.queue_declare(queue=CRAWLER_QUEUE, durable = True)
 channel.queue_declare(queue=SPACY_QUEUE, durable = True)
 channel.basic_consume(queue=CRAWLER_QUEUE, on_message_callback=callback, auto_ack=True)
+
+client = Elasticsearch("http://"+ESENDPOINT+":9200", basic_auth=("elastic", ESPASSWORD), verify_certs=False)
 
 channel.start_consuming()
 
