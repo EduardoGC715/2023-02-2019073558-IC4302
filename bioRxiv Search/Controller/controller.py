45e86fe728f537117f2cff2847eb545ca8bdbb6f
Daniel Granados <dandiegogranados@yahoo.com>
2023-08-17 19:44:57 -0600

    Comentarios
    Se agregaron comentarios en el código del controller y del crawler.

--- bioRxiv Search/Controller/controller.py
+++ bioRxiv Search/Controller/controller.py
@@ -7,6 +7,7 @@ import time
 from elasticsearch import Elasticsearch
 from elasticsearch import TransportError
 
+# función que obtiene extrea datos del API de bioRxiv.
 def get_biorxiv_data():
     base_url = 'https://api.biorxiv.org'
     endpoint = '/covid19/30'
@@ -20,20 +21,24 @@ def get_biorxiv_data():
         print(f"Error: {response.status_code} - {response.text}")
         return None
     
+# Parámetros de conexión con RabbitMQ
 RABBIT_MQ=os.getenv('RABBITMQ')
 RABBIT_MQ_PASSWORD=os.getenv('RABBITPASS')
 CRAWLER_QUEUE=os.getenv('CRAWLER_QUEUE')
 
+# Parámetros de conexión a Elasticsearch
 ESENDPOINT=os.getenv('ESENDPOINT')
 ESPASSWORD=os.getenv('ESPASSWORD')
 ESINDEX=os.getenv('ESINDEX')
 
+# Conexión con RabbitMQ
 credentials = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
 parameters = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
-channel.queue_declare(queue=CRAWLER_QUEUE, durable = True)
+channel.queue_declare(queue=CRAWLER_QUEUE, durable = True) # Cola por la cual se mandan mensajes al crawler.
 
+# Conexión con elasticsearch
 client = Elasticsearch("http://"+ESENDPOINT+":9200", basic_auth=("elastic", ESPASSWORD), verify_certs=False)
 
 index_settings = {
@@ -55,19 +60,19 @@ index_settings = {
     }
 }
 
+# Crea el índice de jobs, o el dado en ESINDEX
 try:
     client.indices.create(index=ESINDEX)
     print(f"Index '{ESINDEX}' created successfully.")
 except Exception as e:
     print(f"Index '{ESINDEX}' already exists.")
 
-
-
-indexes = []
+# loop donde el controller va a revisar el índice por mensajes no procesados.
 while True:
     print("Checking...")
-    # Revisa a que recibe el request del kibana service.
+    # Revisa hasta que recibe el request del kibana service.
     try:
+        # busca las entradas que no estén procesadas.
         response = client.search(index=ESINDEX, query = {"term": {
             "processed": False
         }})
@@ -76,26 +81,27 @@ while True:
         time.sleep(5)
         continue
     
+    # si encontró algún documento
     if len(response['hits']['hits']) != 0:
         print("Found...")
         for hit in response['hits']['hits']:
-            if hit['_source']['jobId'] in indexes:
-                continue
             jsonread = hit['_source']
             pageSize = int(jsonread["pageSize"]) # extrae el page size del índice
             apiData = get_biorxiv_data()
-            total = int(apiData["messages"][0]["total"])
+            total = int(apiData["messages"][0]["total"]) # cantidad total de mensajes
             print(total)
             print(jsonread)
 
-            splits = math.ceil(total / pageSize)
+            splits = math.ceil(total / pageSize) # cantidad total de splits
+
+            # Se procesan los splits para publicarlos en la cola de RabbitMQ
             for split in range(splits):
                 jsonread["splitNumber"] = split
                 msg = json.dumps(jsonread)
                 channel.basic_publish(exchange='', routing_key=CRAWLER_QUEUE, body=msg)
             jsonread['processed'] = True
+            # Se actualiza el documento como procesado,
             client.update(index=ESINDEX, id=hit['_id'], doc= {'processed': True}, refresh = True)
-            indexes.append(jsonread['jobId'])
     time.sleep(5)
 
 connection.close()
