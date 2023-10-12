b5c5bd6c3a88d99ef2706af8092b41dcd0367690
Diegomm146 <79932957+Diegomm146@users.noreply.github.com>
2023-10-12 02:30:25 -0600

    ui search autonomous

--- P1/UI/wikisearch/components/DocumentTable.js
+++ P1/UI/wikisearch/components/DocumentTable.js
@@ -1,17 +1,26 @@
 
 import styles from '../styles/Search.module.css';
 import DocumentPreview from './DocumentPreview';
+import DocumentPreviewAutonomous from './DocumentPreviewAutonomous';
 import { Fragment } from 'react';
 
 
 export default function DocumentTable({ documentList, searchQuery, searchEngine }) {
 
     const documentKeys = Object.keys(documentList);
-    const documentComponents = documentKeys.map((doc) => <DocumentPreview key={documentList[doc]['_id']} doc={documentList[doc]} searchQuery={searchQuery} searchEngine={searchEngine}/>);
+    let documentComponents;
+    if(searchEngine === "SQL"){
+        documentComponents = documentKeys.map((doc) => <DocumentPreviewAutonomous key={documentList[doc]['pageId']} doc={documentList[doc]} searchQuery={searchQuery} searchEngine={searchEngine}/>);
+
+    }else {
+        documentComponents = documentKeys.map((doc) => <DocumentPreview key={documentList[doc]['_id']} doc={documentList[doc]} searchQuery={searchQuery} searchEngine={searchEngine}/>);
+
+    }
     return (
         <div className={styles.documentList}>
             <p className={styles.facetTitle}>Document List</p>
             {documentComponents}
         </div>
     )
+
 }
