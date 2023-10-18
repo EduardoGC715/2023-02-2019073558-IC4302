b181fd3518db65be1426bdfd52f3cea05d98606c
Daniel Granados <dandiegogranados@yahoo.com>
2023-10-18 08:58:34 -0600

    PostGreSQL Backup and Restore

--- TC2/helm/backups/scripts/postgresqlRestore.sh
+++ TC2/helm/backups/scripts/postgresqlRestore.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 # https://devopscube.com/install-configure-postgresql-amazon-linux/
-DATE=$(date '+%Y%m%d%H%M')
+DIRECTORY="/pgdump"
+mkdir -p $DIRECTORY || echo "Failed to create directory"
 
-mkdir -p /pgdump/$DATE
-yum update
+yum update -y
 
 amazon-linux-extras enable postgresql14
 yum clean metadata
 yum install postgresql -y
 
-# echo "$POSTGRES_HOST:$POSTGRES_PORT:$POSTGRES_DB:$POSTGRES_USER:$POSTGRES_PASSWORD" > ~/.pgpass
-# chmod 0600 ~/.pgpass
-
-# echo $PGPASSWORD
-
-aws s3 cp s3://$BUCKET_NAME/$BACKUP_PATH/$RESTORE_FILE /pgdump/$DATE --recursive
+aws s3 cp s3://$BUCKET_NAME/$BACKUP_PATH/$RESTORE_FILE $DIRECTORY
 echo "Downloaded"
 
-psql -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT -w $POSTGRES_DB < /pgdump/$DATE/$RESTORE_FILE
+psql -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT -w $POSTGRES_DB < $DIRECTORY/$RESTORE_FILE
 echo "Restored"
-rm -rf /pgdump/$DATE
+rm -rf $DIRECTORY/$RESTORE_FILE
 echo "Finished!"
