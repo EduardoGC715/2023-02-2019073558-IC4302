88a0d4b5d9cf02d9acc68017627d817af5576f22
Diego-Granados <diegranados@estudiantec.cr>
2023-10-22 19:07:00 -0600

    neo4j community edition

--- TC2/helm/backups/scripts/neo4jBackup.sh
+++ TC2/helm/backups/scripts/neo4jBackup.sh
@@ -3,6 +3,7 @@
 DATE=$(date '+%Y%m%d%H%M')
 
 mkdir -p /neo4j/$DATE
+touch /neo4j/$DATE/backup.cypher
 yum update -y
 
 rpm --import https://debian.neo4j.com/neotechnology.gpg.key
@@ -14,9 +15,14 @@ enabled=1
 gpgcheck=1
 EOF
 
+
 yum install neo4j-5.12.0 -y
 
-pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT -w -c $POSTGRES_DB > /pgdump/$DATE/$POSTGRES_DB-$DATE.sql
+curl -X POST http://databases-admin.default.svc.cluster.local:7474/db/neo4j/tx/commit -H "Content-Type:application/json" -d "{\"statements\":[{\"statement\":\"WITH 'backup.graphml' AS filename CALL apoc.export.graphml.all(filename, {useTypes:TRUE, storeNodeIds:FALSE}) YIELD file RETURN file;\"}]}" -H "Authorization: Basic bmVvNGo6bmVvNGotcGFzc3dvcmQ=" > /neo4j/$DATE/backup.graphml
+
+curl --verbose POST http://databases-admin.default.svc.cluster.local:7474/db/neo4j/tx/commit -H "Content-Type:application/json" -d "{\"statements\":[{\"statement\":\"CALL apoc.export.cypher.all('backup.cypher', {useTypes: TRUE, storeNodeIds: FALSE})\"}]}" -H "Authorization: Basic bmVvNGo6bmVvNGotcGFzc3dvcmQ=" > /neo4j/$DATE/backup.cypher
+
+
 echo "Backed up"
 aws s3 cp /neo4j/$DATE s3://$BUCKET_NAME/$BACKUP_PATH/ --recursive
 aws s3 ls s3://$BUCKET_NAME/$BACKUP_PATH/
