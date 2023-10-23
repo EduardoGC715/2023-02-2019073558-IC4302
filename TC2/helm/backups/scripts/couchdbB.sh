c1da0f6a049aaf7d4c4a0b9d3991651a3321a9af
Diegomm146 <79932957+Diegomm146@users.noreply.github.com>
2023-10-22 19:32:12 -0600

    restore couchdb

--- TC2/helm/backups/scripts/couchdbB.sh
+++ TC2/helm/backups/scripts/couchdbB.sh
@@ -7,8 +7,8 @@ yum install php-curl -y
 yum install jq -y
 
 URL="http://$COUCHDB_USER:$COUCHDB_PSW@$COUCHDB_HOST:$COUCHDB_PORT/$COUCHDB_DB/_all_docs?include_docs=true"
-curl -X GET "$URL" -H "Accept: application/json" | jq '{ docs: [.rows[] | .doc] }' > /couchdump/$DATE/pokemonBackup.json
-echo "Backed up"
+curl -X GET "$URL" -H "Accept: application/json" | jq '{ docs: [.rows[] | .doc] }' > /couchdump/$DATE/couchdb-$DATE.json
+echo "Backed up couchdb-$DATE.json"
 
 aws s3 cp /couchdump/$DATE s3://$BUCKET_NAME/$BACKUP_PATH/ --recursive
 aws s3 ls s3://$BUCKET_NAME/$BACKUP_PATH/
--- TC2/helm/backups/scripts/couchdbR.sh
+++ TC2/helm/backups/scripts/couchdbR.sh
@@ -7,6 +7,7 @@ mkdir -p $DIRECTORY || echo "Failed to create directory"
 # Install necessary tools
 yum update -y
 yum install php-curl -y
+yum install jq -y
 
 # Download the specified backup file from S3 to the local directory
 aws s3 cp s3://$BUCKET_NAME/$BACKUP_PATH/$RESTORE_FILE $DIRECTORY
@@ -15,8 +16,11 @@ echo "Downloaded"
 # The file will be stored in the DIRECTORY, so we set the full path to the downloaded file.
 FILE_PATH="$DIRECTORY/$RESTORE_FILE"
 
+# Remove _rev fields using jq
+jq 'del(.docs[]._rev)' $FILE_PATH > "${FILE_PATH}_processed"
+mv "${FILE_PATH}_processed" $FILE_PATH
+
 # Restore the database
-# Assuming your CouchDB supports the _bulk_docs endpoint for batch insertion
 URL="http://$COUCHDB_USER:$COUCHDB_PSW@$COUCHDB_HOST:$COUCHDB_PORT/$COUCHDB_DB/_bulk_docs"
 curl -X POST "$URL" -H "Content-Type: application/json" --data "@$FILE_PATH"
 echo "Restored"
