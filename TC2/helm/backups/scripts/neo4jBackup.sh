7167a557918ce0840419080538a899dd27c48feb
Diego-Granados <diegranados@estudiantec.cr>
2023-10-23 15:04:22 -0600

    Neo4J Backup and restore

--- TC2/helm/backups/scripts/neo4jBackup.sh
+++ TC2/helm/backups/scripts/neo4jBackup.sh
@@ -3,7 +3,7 @@
 DATE=$(date '+%Y%m%d%H%M')
 
 mkdir -p /neo4j/$DATE
-touch /neo4j/$DATE/backup.cypher
+
 yum update -y
 
 rpm --import https://debian.neo4j.com/neotechnology.gpg.key
@@ -15,13 +15,9 @@ enabled=1
 gpgcheck=1
 EOF
 
+NEO4J_ACCEPT_LICENSE_AGREEMENT=yes yum install neo4j-enterprise-5.13.0 -y
 
-yum install neo4j-5.12.0 -y
-
-curl -X POST http://databases-admin.default.svc.cluster.local:7474/db/neo4j/tx/commit -H "Content-Type:application/json" -d "{\"statements\":[{\"statement\":\"WITH 'backup.graphml' AS filename CALL apoc.export.graphml.all(filename, {useTypes:TRUE, storeNodeIds:FALSE}) YIELD file RETURN file;\"}]}" -H "Authorization: Basic bmVvNGo6bmVvNGotcGFzc3dvcmQ=" > /neo4j/$DATE/backup.graphml
-
-curl --verbose POST http://databases-admin.default.svc.cluster.local:7474/db/neo4j/tx/commit -H "Content-Type:application/json" -d "{\"statements\":[{\"statement\":\"CALL apoc.export.cypher.all('backup.cypher', {useTypes: TRUE, storeNodeIds: FALSE})\"}]}" -H "Authorization: Basic bmVvNGo6bmVvNGotcGFzc3dvcmQ=" > /neo4j/$DATE/backup.cypher
-
+neo4j-admin database backup --from=$NEO4J_SERVICE:$NEO4J_PORT --type=full --to-path=/neo4j/$DATE
 
 echo "Backed up"
 aws s3 cp /neo4j/$DATE s3://$BUCKET_NAME/$BACKUP_PATH/ --recursive
